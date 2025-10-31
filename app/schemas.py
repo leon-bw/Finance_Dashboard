cb2e6a0b4bf07add4c3aac4457458d20950b1765
Leon Williams <leonbwilliams6@gmail.com>
2025-10-31 10:44:49 +0000

    Add budget and category class models

--- app/schemas.py
+++ app/schemas.py
@@ -2,9 +2,9 @@ from datetime import datetime
 from typing import Literal, Optional
 from uuid import UUID
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, ValidationInfo, field_validator
 
-# Transaction class models
+# ----Transaction class models----
 
 
 class TransactionBase(BaseModel):
@@ -67,7 +67,7 @@ class TransactionUpdate(TransactionBase):
     status: Optional[Literal["completed", "pending"]] = None
 
 
-# User class models
+# ----User class models----
 
 
 class UserBase(BaseModel):
@@ -139,7 +139,7 @@ class UserResponse(BaseModel):
         from_attributes = True
 
 
-# JWT Token class models
+# ----JWT Token class models----
 
 
 class Token(BaseModel):
@@ -158,3 +158,136 @@ class TokenData(BaseModel):
 
     username: Optional[str] = None
     user_id: Optional[UUID] = None
+
+
+# ----Category class models----
+
+
+class CategoryBase(BaseModel):
+    """
+    Base model with shared fields for categories
+    """
+
+    name: str = Field(
+        min_length=1, max_length=50, description="The name of the category"
+    )
+    type: Literal["income", "expense"] = Field(description="THe type of the category")
+    description: Optional[str] = Field(description="The description of the category")
+    colour: Optional[str] = Field(description="The colour of the category")
+    icon: Optional[str] = Field(description="The icon of the category")
+
+
+class CategoryCreate(CategoryBase):
+    """
+    Model inherits from CategoryBase for creating a new category
+    """
+
+    pass
+
+
+class CategoryResponse(CategoryBase):
+    """
+    Model for properties returned to client
+    """
+
+    id: UUID = Field(description="The unique identifier of the category")
+    is_default: bool = Field(description="Whether this is a default category or not")
+    created_at: datetime = Field(
+        description="The date and time the category was created in the database"
+    )
+
+
+class CategoryUpdate(CategoryBase):
+    """
+    Model inherits from CategoryBase for updating categories
+    """
+
+    name: Optional[str] = None
+    description: Optional[str] = None
+    icon: Optional[str] = None
+    colour: Optional[str] = None
+
+    class Config:
+        from_attributes = True
+
+
+# ----Budget class models----
+
+
+class BudgetBase(BaseModel):
+    """
+    Base model with shared fields for budgets
+    """
+
+    amount: float = Field(gt=0, description="The limit of the budget")
+    period: Literal["daily", "weekly", "monthly", "yearly"] = Field(
+        default="monthly", description="The period of the budget"
+    )
+    category_id: Optional[UUID] = Field(
+        description="The category the budget belongs to"
+    )
+    start_date: datetime = Field(description="The start date of the budget")
+    end_date: datetime = Field(description="The end date of the budget")
+    alert_threshold: float = Field(
+        default=0.8,
+        ge=0.0,
+        le=1.0,
+        description="The alert threshold when (%) of budget is spent",
+    )
+
+    @field_validator("end_date")
+    @classmethod
+    def end_date_must_be_after_start_date(cls, value, info: ValidationInfo):
+        # Validate that the end date is after start date
+        if value is not None and "start_date" in info.data:
+            if value <= info.data["start_date"]:
+                raise ValueError("End date must be after start date")
+        return value
+
+
+class BudgetCreate(BudgetBase):
+    """
+    Model inherits from BudgetBase for creating a new budget
+    """
+
+    pass
+
+
+class BudgetResponse(BudgetBase):
+    """
+    Model for properties returned to client
+    """
+
+    id: UUID = Field(description="The unique identifier of the budget")
+    user_id: UUID = Field(description="The user the budget belongs to")
+    is_active: bool = Field(
+        default=True, description="Whether the budget is active or not"
+    )
+    spent_amount: Optional[float] = Field(
+        description="The current amount spent of the budget"
+    )
+    remaining_amount: Optional[float] = Field(
+        description="The amount of budget remaining"
+    )
+    percentage_spent: Optional[float] = Field(
+        description="The percentage of the budget that has been used"
+    )
+    category: Optional[CategoryResponse] = Field(
+        None, description="The category the budget belongs to"
+    )
+
+    class Config:
+        from_attributes = True
+
+
+class BudgetUpdate(BudgetBase):
+    """
+    Model inherits from BudgetBase for updating budgets
+    """
+
+    amount: Optional[float] = Field(None, gt=0)
+    period: Optional[Literal["daily", "weekly", "monthly", "yearly"]] = None
+    start_date: Optional[datetime] = None
+    end_date: Optional[datetime] = None
+    is_active: Optional[bool] = None
+    alert_threshold: Optional[float] = Field(None, ge=0.0, le=1.0)
